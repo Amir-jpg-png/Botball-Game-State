30ac7dcff8786188fae837e4856b6225dcd77fbc
Amir Elmesiry <mrelmesiry@gmail.com>
2026-03-04 19:30:40 +0100

    remove .idea, remove install script and later add it to main branch

--- installOnWombat.sh
+++
@@ -1,3 +0,0 @@
-echo "deb [trusted=yes] https://git.tech-support.systems/TechSupport/game-state/raw/branch/apt-repository ./" | sudo tee /etc/apt/sources.list.d/botball.list
-sudo apt update
-sudo apt install botball-gamestate
